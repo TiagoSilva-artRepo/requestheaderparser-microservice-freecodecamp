0961555066db62c40bcd8af61f81e00dc24dc0b3
Tiago Silva <tiago.silva@contisystems.pt>
2021-11-12 11:19:10 +0000

    add ipaddress, language and software to response

--- server.js
+++ server.js
@@ -21,10 +21,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.json({ipaddress: req.ip, language: req.acceptsLanguages, software: req.headers["user-agent"]});
 });
-
 
 
 // listen for requests :)
